2048d705f33b9ec1eef75de3e1d4ba067d10d6a8
Pooja Jogi <pooja.jogi@slksoft.com>
2018-08-10 16:53:10 +0530

    first

---
+++ WebApplication3/Controllers/registrationsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication3;
+
+namespace WebApplication3.Controllers
+{
+    public class registrationsController : Controller
+    {
+        private SLKDatabaseEntities1 db = new SLKDatabaseEntities1();
+
+        // GET: registrations
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.registrations.ToListAsync());
+        }
+
+        // GET: registrations/Details/5
+        public async Task<ActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            registration registration = await db.registrations.FindAsync(id);
+            if (registration == null)
+            {
+                return HttpNotFound();
+            }
+            return View(registration);
+        }
+
+        // GET: registrations/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: registrations/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "SlNo,UserName,EmailId,Password,ConfirmPassword")] registration registration)
+        {
+            if (ModelState.IsValid)
+            {
+                db.registrations.Add(registration);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(registration);
+        }
+
+        // GET: registrations/Edit/5
+        public async Task<ActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            registration registration = await db.registrations.FindAsync(id);
+            if (registration == null)
+            {
+                return HttpNotFound();
+            }
+            return View(registration);
+        }
+
+        // POST: registrations/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "SlNo,UserName,EmailId,Password,ConfirmPassword")] registration registration)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(registration).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(registration);
+        }
+
+        // GET: registrations/Delete/5
+        public async Task<ActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            registration registration = await db.registrations.FindAsync(id);
+            if (registration == null)
+            {
+                return HttpNotFound();
+            }
+            return View(registration);
+        }
+
+        // POST: registrations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(string id)
+        {
+            registration registration = await db.registrations.FindAsync(id);
+            db.registrations.Remove(registration);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
